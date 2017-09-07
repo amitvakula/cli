9bb680cbee098f238912b573a8933bd40b6cd9b2
Nathaniel Kofalt <nathaniel@kofalt.com>
2017-09-07 14:24:15 -0500

    Add hostname to fw-status

--- ops/status.go
+++ ops/status.go
@@ -3,7 +3,9 @@ package ops
 import (
 	. "fmt"
 	"os"
+	"strings"
 
+	. "flywheel.io/fw/util"
 	"flywheel.io/sdk/api"
 )
 
@@ -23,5 +25,11 @@ func Status(client *api.Client) {
 		os.Exit(1)
 	}
 
-	Println("You are currently logged in as", user.Firstname, user.Lastname+"!")
+	// Shenanigans: grab the URL string and drop the API prefix for a convenient browser URL
+	req, err := client.Sling.Request()
+	Check(err)
+	hostname := strings.TrimSuffix(req.URL.String(), "/api/")
+	hostname = strings.TrimSuffix(hostname, ":443")
+
+	Println("You are currently logged in as", user.Firstname, user.Lastname, "to", hostname)
 }

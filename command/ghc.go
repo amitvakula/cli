cb989fa3c74e33894daac17192b3014d4a3c1d07
David Farkas <david.farkas93@gmail.com>
2018-09-18 16:29:41 +0200

    Add dependency and ghc import command

--- command/ghc.go
+++ command/ghc.go
@@ -16,9 +16,17 @@ type queryRequest struct {
 }
 
 type queryResult struct {
-	JobId   string `json:"jobId"`
-	Rows [][]string `json:"rows"`
-	Colums []string `json:"columns"`
+	JobId   string     `json:"jobId"`
+	Rows    [][]string `json:"rows"`
+	Columns []string   `json:"columns"`
+}
+
+type importJobRequest struct {
+	JobId string `json:"jobId"`
+}
+
+type importJobResponse struct {
+	Id string `json:"_id"`
 }
 
 func (o *opts) ghcCommand() *cobra.Command {
@@ -28,13 +36,14 @@ func (o *opts) ghcCommand() *cobra.Command {
 	}
 
 	cmd.AddCommand(o.bigQueryCommand())
+	cmd.AddCommand(o.importJobCommand())
 
 	return cmd
 }
 
 func (o *opts) bigQueryCommand() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "query [DATASET] [TABLE] [SQL:WHERE]",
+		Use:    "query [DATASET] [TABLE] [SQL WHERE CLAUSE]",
 		Short:  "Run a query",
 		Args:   cobra.ExactArgs(3),
 		PreRun: o.RequireClient,
@@ -42,7 +51,7 @@ func (o *opts) bigQueryCommand() *cobra.Command {
 			res1D := &queryRequest{
 				Where:   args[2],
 				Table:   args[1],
-				Dataset:   args[0]}
+				Dataset: args[0]}
 			log.Println("Running big query")
 
 			var aerr *api.Error
@@ -50,16 +59,39 @@ func (o *opts) bigQueryCommand() *cobra.Command {
 
 			o.Client.New().Post("ghc/query").BodyJSON(res1D).Receive(&response, &aerr)
 
-			fmt.Printf("Job Id: %s\n", response.JobId)
+			fmt.Printf("Query Job Id: %s\n", response.JobId)
 
 			table := tablewriter.NewWriter(os.Stdout)
-			table.SetHeader(response.Colums)
+			table.SetHeader(response.Columns)
 
 			for _, row := range response.Rows {
 				table.Append(row)
 			}
 
 			table.Render()
+		},
+	}
+
+	return cmd
+}
+
+func (o *opts) importJobCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:    "import [JOB_ID]",
+		Short:  "Importing the results of a query job",
+		Args:   cobra.ExactArgs(1),
+		PreRun: o.RequireClient,
+		Run: func(cmd *cobra.Command, args []string) {
+			res1D := &importJobRequest{
+				JobId: args[0]}
+			log.Printf("Import %s job\n", res1D.JobId)
+
+			var aerr *api.Error
+			var response *importJobResponse
+
+			o.Client.New().Post("ghc/import").BodyJSON(res1D).Receive(&response, &aerr)
+
+			fmt.Printf("FW Job Id: %s\n", response.Id)
 		},
 	}
 

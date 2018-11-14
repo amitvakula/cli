0943f97ae6f7b439a3fbdc5a092389b0db420fe0
David Farkas <davidfarkas@invenshure.com>
2018-11-14 19:40:47 +0100

    Refactor gcp config mechanism

--- command/delegate.go
+++ command/delegate.go
@@ -38,6 +38,7 @@ var DelegatedCommands = []CommandDesc{
 	{"import", "Import data into Flywheel"},
 	{"export", "Export data from Flywheel"},
 	{"ghc", "Query and import dicom files from GHC"},
+	{"gcp", "Google Cloud platform releated commands, set default configs etc."},
 	{"data-view", "Data views"},
 }
 

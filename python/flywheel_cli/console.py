dcd7b193f06845c2da1fffb05b721997b81f55c8
Justin Ehlert <justinehlert@flywheel.io>
2019-04-09 15:54:56 -0500

    Support NO_COLOR environment variable

--- python/flywheel_cli/console.py
+++ python/flywheel_cli/console.py
@@ -1,4 +1,10 @@
 """Provide helpers for printing to the console"""
+import os
+import sty
+
+
+NO_COLOR=None
+
 
 def print_table(out, table):
     # Calculate max width
@@ -12,3 +18,22 @@ def print_table(out, table):
             print(col.ljust(widths[i]), end=' ', file=out)
         print(file=out)
 
+
+def green_bold(s):
+    return style(s, sty.fg.green + sty.ef.bold, sty.rs.bold_dim + sty.rs.fg)
+
+
+def blue_bold(s):
+    return style(s, sty.fg.blue + sty.ef.bold, sty.rs.bold_dim + sty.rs.fg)
+
+
+def style(s, sty_set, sty_unset):
+    global NO_COLOR
+
+    if NO_COLOR is None:
+        NO_COLOR = 'NO_COLOR' in os.environ
+
+    if NO_COLOR:
+        return s
+
+    return sty_set + s + sty_unset

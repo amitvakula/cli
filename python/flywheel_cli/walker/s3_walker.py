ce1aedb2f4cdd904b4ea7c92d23b0173e97a2646
edgrams <edgramswork@gmail.com>
2019-05-01 10:44:33 -0500

    lint cleanup

--- python/flywheel_cli/walker/s3_walker.py
+++ python/flywheel_cli/walker/s3_walker.py
@@ -10,11 +10,11 @@ from .abstract_walker import AbstractWalker, FileInfo
 class S3Walker(AbstractWalker):
     """Walker that is implemented in terms of S3"""
     def __init__(self, fs_url, ignore_dot_files=True, follow_symlinks=False, filter=None, exclude=None,
-                 filter_dirs=None, exclude_dirs=None, src_fs=None):
+                 filter_dirs=None, exclude_dirs=None):
         """Initialize the abstract walker
 
         Args:
-            root (str): The starting directory for walking
+            fs_url (str): The starting directory for walking
             ignore_dot_files (bool): Whether or not to ignore files starting with '.'
             follow_symlinks(bool): Whether or not to follow symlinks
             filter (list): An optional list of filename patterns to INCLUDE
@@ -28,41 +28,41 @@ class S3Walker(AbstractWalker):
         sanitized_path = '' if path == '/' else path[:-1]
 
         super(S3Walker, self).__init__(sanitized_path, ignore_dot_files=ignore_dot_files,
-                follow_symlinks=follow_symlinks, filter=filter, exclude=exclude,
-                filter_dirs=filter_dirs, exclude_dirs=exclude_dirs)
+                                       follow_symlinks=follow_symlinks, filter=filter, exclude=exclude,
+                                       filter_dirs=filter_dirs, exclude_dirs=exclude_dirs)
         self.base_path = sanitized_path
         self.bucket = bucket
         self.client = boto3.client('s3')
         self.fs_url = fs_url
+        self.tmp_dir_path = None
 
     def get_fs_url(self):
         return self.fs_url
 
     def close(self):
-        if self.dirpath:
-            shutil.rmtree(self.dirpath)
+        if self.tmp_dir_path is not None:
+            shutil.rmtree(self.tmp_dir_path)
 
     def open(self, path, mode='rb', **kwargs):
-        if not hasattr(self, 'dirpath'):
-            self.dirpath = tempfile.mkdtemp()
+        if self.tmp_dir_path is None:
+            self.tmp_dir_path = tempfile.mkdtemp()
 
         # need to handle the case where there is no prefix path
         prefix_dir = path.rsplit('/', 1)[0]
-        file_dir = self.dirpath + self.base_path + prefix_dir
+        file_dir = self.tmp_dir_path + self.base_path + prefix_dir
 
         if not os.path.isdir(file_dir):
             os.makedirs(file_dir)
 
-        file_path = self.dirpath + self.base_path + path
+        file_path = self.tmp_dir_path + self.base_path + path
         prefix_path = (self.base_path + path)[1:]
 
         self.client.download_file(self.bucket, prefix_path, file_path)
 
         try:
             return open(file_path, mode, **kwargs)
-        except fs.errors.ResourceNotFound as ex:
+        except fs.errors.ResourceNotFound:
             raise FileNotFoundError('File {} not found'.format(path))
-
 
     def _listdir(self, path):
         prefix_path = path[1:] if path.endswith('/') else path[1:] + '/'

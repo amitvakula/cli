50695284e40e23d3e0774b5e2991b90b288760c7
edgrams <edgramswork@gmail.com>
2019-05-08 09:56:53 -0500

    mock clean up

--- python/tests/unit_tests/walker/test_factory.py
+++ python/tests/unit_tests/walker/test_factory.py
@@ -7,22 +7,30 @@ from flywheel_cli.walker import factory, PyFsWalker, S3Walker
 
 @pytest.fixture
 def mocked_urlparse():
-    mocked_urlparse_patch = mock.patch('flywheel_cli.walker.factory.urlparse')
-    yield mocked_urlparse_patch.start()
+    mocked_urlparse_patch = None
 
-    mocked_urlparse_patch.stop()
+    def patcher(return_value):
+        mocked_urlparse_patch = mock.patch('flywheel_cli.walker.s3_walker.urlparse')
+        mocked_urlparse = mocked_urlparse_patch.start()
+        mocked_urlparse.return_value = return_value
+        return mocked_urlparse
+
+    yield patcher
+
+    if mocked_urlparse_patch is not None:
+        mocked_urlparse_patch.stop()
 
 
 def test_create_walker_should_request_urlparse(mocked_urlparse):
-    mocked_urlparse.return_value = ('s3', 'bucket', 'path')
+    mock_urlparse = mocked_urlparse(('s3', 'bucket', 'path'))
 
     factory.create_walker('s3://bucket/path/')
 
-    mocked_urlparse.assert_called_with('s3://bucket/path/')
+    mock_urlparse.assert_called_with('s3://bucket/path/')
 
 
 def test_create_walker_should_create_s3_walker_instance_for_s3_scheme(mocked_urlparse):
-    mocked_urlparse.return_value = ('s3', 'bucket', 'path')
+    mocked_urlparse(('s3', 'bucket', 'path'))
 
     result = factory.create_walker('s3://bucket/path/')
 
@@ -30,7 +38,7 @@ def test_create_walker_should_create_s3_walker_instance_for_s3_scheme(mocked_url
 
 
 def test_create_walker_should_create_pyfs_walker_instance_for_os_scheme(mocked_urlparse):
-    mocked_urlparse.return_value = ('osfs', '/', '/')
+    mocked_urlparse(('osfs', '/', '/'))
 
     result = factory.create_walker('osfs://')
 
